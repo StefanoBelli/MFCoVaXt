9f5b964f5d8fa54213dba8f74f9d6678ea56eac3
StefanoBelli <stefano9913@gmail.com>
2021-06-13 14:55:02 +0200

    finished layout

---
+++ app/src/main/java/it/mobileflow/mfcovaxt/activity/InjectionLocationActivity.kt
@@ -0,0 +1,12 @@
+package it.mobileflow.mfcovaxt.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import it.mobileflow.mfcovaxt.R
+
+class InjectionLocationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_injection_location)
+    }
+}
