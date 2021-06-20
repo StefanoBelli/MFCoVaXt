d42f31e45a202a81ca6a88ac47db10982d9495d6
StefanoBelli <stefano9913@gmail.com>
2021-06-20 12:03:52 +0200

    first plot ok

--- app/src/main/java/it/mobileflow/mfcovaxt/activity/DataPlotActivity.kt
+++ app/src/main/java/it/mobileflow/mfcovaxt/activity/DataPlotActivity.kt
@@ -1,5 +1,6 @@
 package it.mobileflow.mfcovaxt.activity
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,6 +8,8 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.TextView
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.MarkerView
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
@@ -21,6 +24,8 @@ import java.util.*
 
 class DataPlotActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDataPlotBinding
+    private lateinit var areas: Array<String>
+    private lateinit var chart: LineChart
     private val valueFormatter = object : ValueFormatter() {
         override fun getFormattedValue(value: Float): String {
             return EzDateParser.fmtLongToOnlyDateStr(value.toLong(), baseContext)
@@ -32,8 +37,14 @@ class DataPlotActivity : AppCompatActivity() {
         binding = ActivityDataPlotBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.byVaxSpinner.adapter = getSimpleArrayAdapter(CommonDataHolder.vaxes)
-        binding.byAgeRangeSpinner.adapter = getSimpleArrayAdapter(CommonDataHolder.ageRanges)
+        chart = binding.plotLc
+
+        areas = resources.getStringArray(R.array.areas)
+
+        val vaxesOpts = setOf(getString(R.string.all_vaxes)) + CommonDataHolder.vaxes
+        val ageRangesOpts = setOf(getString(R.string.all_ageranges)) + CommonDataHolder.ageRanges
+        binding.byVaxSpinner.adapter = getSimpleArrayAdapter(vaxesOpts)
+        binding.byAgeRangeSpinner.adapter = getSimpleArrayAdapter(ageRangesOpts)
 
         setLineChart()
         setListeners()
@@ -53,8 +64,6 @@ class DataPlotActivity : AppCompatActivity() {
     }
 
     private fun setLineChart() {
-        val chart = binding.plotLc
-
         chart.axisRight.setDrawGridLines(false)
         chart.axisLeft.setDrawGridLines(false)
         chart.xAxis.setDrawGridLines(false)
@@ -69,37 +78,34 @@ class DataPlotActivity : AppCompatActivity() {
         chart.legend.isEnabled = false
         chart.axisRight.setDrawAxisLine(false)
         chart.axisLeft.setDrawAxisLine(true)
+        chart.axisLeft.labelCount = 6
         chart.xAxis.setDrawLabels(true)
+        chart.xAxis.labelCount = 5
         chart.xAxis.position = XAxis.XAxisPosition.BOTTOM
         chart.axisLeft.setDrawLabels(true)
         chart.axisRight.setDrawLabels(false)
         chart.xAxis.setDrawAxisLine(false)
         chart.xAxis.valueFormatter = valueFormatter
         chart.setScaleEnabled(false)
-        chart.animateX(2000)
+        chart.animateY(2000)
         chart.setNoDataText(getString(R.string.waiting_for_data))
         chart.isHighlightPerDragEnabled = false
         chart.defaultFocusHighlightEnabled = false
         chart.isHighlightPerTapEnabled = false
-
-        /*
-        val entries = mutableListOf(Entry(1f,2f), Entry(2f,4f), Entry(3f,2f), Entry(4f,5f))
-        val dataset = LineDataSet(entries, "ciao")
-
-        dataset.color = getColor(R.color.blue)
-        dataset.setCircleColor(getColor(R.color.darker_blue))
-        chart.data = LineData(dataset)*/
     }
 
-    private fun getSimpleArrayAdapter(values: Set<String>) : ArrayAdapter<String> {
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item,
-            values.toTypedArray())
+    private fun getSimpleArrayAdapter(values: Set<String>): ArrayAdapter<String> {
+        val adapter = ArrayAdapter(
+            this, android.R.layout.simple_spinner_item,
+            values.toTypedArray()
+        )
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         return adapter
     }
 
-    private fun updatePlot(dataIndex: Int, area: Int, ageRange: String, vax: String) {
-        when(dataIndex) {
+    private fun updatePlot(dataIndex: Int, area: Int, ageRange: String, vax: String?) {
+        Log.e("MFCoVaXt", dataIndex.toString())
+        when (dataIndex) {
             0 -> updatePlotTotalInjsData(area, ageRange, vax)
             1 -> updatePlotFirstInjsData(area, ageRange, vax)
             2 -> updatePlotSecondInjsData(area, ageRange, vax)
@@ -107,20 +113,56 @@ class DataPlotActivity : AppCompatActivity() {
         }
     }
 
-    private fun updatePlotVaxDeliveryData(area: Int, vax: String) {
+    private fun updatePlotVaxDeliveryData(area: Int, vax: String?) {
+        val filtered = CommonDataHolder.vaxDeliveries
+
+        val dates: MutableSet<Date> = mutableSetOf()
+        for (delivery in CommonDataHolder.vaxDeliveries) {
+            dates += delivery.deliveryDate
+        }
+
+        val plotEntries : MutableList<Entry> = mutableListOf()
+        val delivForDate : MutableMap<Date, Int> = mutableMapOf()
+        var totalDeliveries = 0
+
+        for (date in dates.sorted()) {
+            filtered.filter {
+                it.deliveryDate == date
+            }.filter {
+                if(vax != null) it.vaxName == vax else true
+            }.filter {
+                if(area > 0) areas[area] == it.areaName else true
+            }.forEach {
+                totalDeliveries += it.numOfVaxes
+            }
+
+            delivForDate[date] = totalDeliveries
+            plotEntries.add(Entry(date.time.toFloat(), delivForDate[date]!!.toFloat()))
+        }
+
+        applyNewDataset(R.string.vax_availability, plotEntries)
+    }
+
+    private fun updatePlotSecondInjsData(area: Int, ageRange: String, vax: String?) {
 
     }
 
-    private fun updatePlotSecondInjsData(area: Int, ageRange: String, vax: String) {
+    private fun updatePlotFirstInjsData(area: Int, ageRange: String, vax: String?) {
 
     }
 
-    private fun updatePlotFirstInjsData(area: Int, ageRange: String, vax: String) {
+    private fun updatePlotTotalInjsData(area: Int, ageRange: String, vax: String?) {
 
     }
 
-    private fun updatePlotTotalInjsData(area: Int, ageRange: String, vax: String) {
+    private fun applyNewDataset(labelRes: Int, entries: List<Entry>) {
+        val dataset = LineDataSet(entries, getString(labelRes))
+        dataset.color = getColor(R.color.blue)
+        dataset.setDrawCircleHole(false)
+        dataset.setDrawCircles(false)
 
+        chart.data = LineData(dataset)
+        chart.invalidate()
     }
 
     open inner class ItemSelectedListener : AdapterView.OnItemSelectedListener {
@@ -129,7 +171,11 @@ class DataPlotActivity : AppCompatActivity() {
                 binding.dataTypeSpinner.selectedItemPosition,
                 binding.byAreaSpinner.selectedItemPosition,
                 (binding.byAgeRangeSpinner.selectedView as TextView).text.toString(),
-                (binding.byVaxSpinner.selectedView as TextView).text.toString())
+                if(binding.byVaxSpinner.selectedItemPosition == 0)
+                    null
+                else
+                    (binding.byVaxSpinner.selectedView as TextView).text.toString()
+            )
         }
 
         override fun onNothingSelected(parent: AdapterView<*>?) {}
